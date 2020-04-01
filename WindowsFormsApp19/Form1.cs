0aebd9f118ccebaf1770c7339662963bfa79d629
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-04-01 15:39:53 +0500

    Changes

--- WindowsFormsApp19/Form1.cs
+++ WindowsFormsApp19/Form1.cs
@@ -22,13 +22,27 @@ namespace WindowsFormsApp19
             Graphics g = this.CreateGraphics();
             g.Clear(Color.White);
             Pen pen = new Pen(Color.Aquamarine);
-            pen.Width = 20;
+            Brush brush = new SolidBrush(Color.Orange);
+            pen.Width = 3;
             int[] a = new int[10];
             Random r = new Random();
             for (int i = 0; i < 10; i++)
             {
                 a[i] = r.Next(50, 350);
-                g.DrawLine(pen, 50 + 70 * i, 400, 50 + 70 * i,400-a[i]);
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                g.DrawLine(pen, 50 + 70 * i, 400 -a[i],50 + 70 * (i + 1),400 -a[i+1]);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                g.FillEllipse(brush, 50 + 70 * i-5, 400 - a[i]-5,10,10);
+            }
+            Brush brush1 = new SolidBrush(Color.Black);
+            Font font = new Font(new FontFamily("Corbel"), 20);
+            for (int i = 0; i < 10; i++)
+            {
+                g.DrawString(a[i].ToString(), font, brush1, 50 + 70 * i-25, 400 - a[i] - 35);
             }
         }
     }
